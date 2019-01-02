93e5a9d0eb62ba4985527eda29ea019804b94a77
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-02 00:43:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
     count = 0
     board.each do |item|
-        if item != " " || item != ""
+        if item == "X" || item == "O"
             count += 1
         end
     end
